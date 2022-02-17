8547a35dbb5eba932ec3b53a92baf150d8dc9912
Simon Costea <simoncostea@yahoo.com>
2022-02-17 10:34:15 +0200

    Remove duplicated console log

--- bundler/webpack.dev.js
+++ bundler/webpack.dev.js
@@ -3,11 +3,6 @@ const { merge } = require('webpack-merge')
 const commonConfiguration = require('./webpack.common.js')
 const portFinderSync = require('portfinder-sync')
 
-const infoColor = (_message) =>
-{
-    return `\u001b[1m\u001b[34m${_message}\u001b[39m\u001b[22m`
-}
-
 module.exports = merge(
     commonConfiguration,
     {
@@ -32,16 +27,6 @@ module.exports = merge(
                 logging: 'none',
                 overlay: true,
                 progress: false
-            },
-            onAfterSetupMiddleware: function(devServer)
-            {
-                const port = devServer.options.port
-                const https = devServer.options.https ? 's' : ''
-                const localIp = 'localhost'
-                const domain1 = `http${https}://${localIp}:${port}`
-                const domain2 = `http${https}://localhost:${port}`
-                
-                console.log(`Project running at:\n  - ${infoColor(domain1)}\n  - ${infoColor(domain2)}`)
             }
         }
     }
