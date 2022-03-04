7664707a3432ae1db547afae0ed18a38b121efae
Bogdan Niculae <bogdaniculae@gmail.com>
2022-03-04 16:53:47 +0200

    add lookat label debug

--- src/Experience/Camera.js
+++ src/Experience/Camera.js
@@ -2,6 +2,7 @@ import * as THREE from 'three'
 import Experience from "./Experience.js";
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
 import TWEEN from '@tweenjs/tween.js'
+import LabeledSphere3D from './Components/LabeledSphere3D.js'
 
 let camera = null
 
@@ -14,50 +15,25 @@ export default class Camera {
         this.debug = this.experience.debug
         camera = this
 
-        this.views = [
-            {
-                name: 'default',
-                left: 0,
-                bottom: 0,
-                width: 0.5,
-                height: 1.0,
-                background: new THREE.Color(0xff0000),
-                eye: [0, 5, 20],
-                up: [0, 1, 0],
-                fov: 60,
-                updateCamera: (camera, scene) => {
-                    camera.lookAt(scene.position)
-                }
-            },
-            {
-                name: 'debug',
-                left: 0.5,
-                bottom: 0,
-                width: 0.5,
-                height: 1.0,
-                background: new THREE.Color(0x00ff00),
-                eye: [0, 5, 20],
-                up: [0, 1, 0],
-                fov: 75,
-                updateCamera: (camera, scene) => {
-                    camera.lookAt(scene.position)
-                }
-            }
-        ]
-
         // Options
         this.data = {
-            fov: 60,
-            aspect: this.sizes.width / this.sizes.height,
-            near: 0.1,
-            far: 1000,
-            moveDuration: 1200,
-
             position: new THREE.Vector3(0, 1.7, 10),
             lookAt: new THREE.Vector3(0, 1.7, 0),
+            moveDuration: 1200,
+            zoom: 1.15,
             location: 0,
             debug: false,
         }
+
+        this.lookAtLabel3D = new LabeledSphere3D({
+            labelText: '(Look at)',
+            size: 0.05,
+            color: new THREE.Color().setRGB(0.65, 0.792, 0.219)
+        })
+
+        this.scene.add(this.lookAtLabel3D)
+
+        console.log(this.lookAtLabel3D);
 
         this.cameraLocations = [
             {
@@ -100,15 +76,13 @@ export default class Camera {
         this.setInstance()
         this.setOrbitControls()
         this.autoRotateControls()
+
+        if (this.debug.active)
+            this.addGUIControls()
     }
 
     setInstance() {
-        this.instance = new THREE.PerspectiveCamera(
-            this.data.fov,
-            this.data.aspect,
-            this.data.near,
-            this.data.far
-        )
+        this.instance = new THREE.PerspectiveCamera(60, this.sizes.width / this.sizes.height, 0.01, 1000)
         this.instance.position.copy(this.data.position)
         this.scene.add(this.instance)
     }
@@ -116,7 +90,7 @@ export default class Camera {
     setOrbitControls() {
         this.controls = new OrbitControls(this.instance, this.canvas)
         this.controls.target.copy(this.data.lookAt)
-        this.updateOrbitControls()
+        this.controls.update()
     }
 
     updateOrbitControls() {
@@ -171,16 +145,16 @@ export default class Camera {
                 )
             })
             .start()
-        
-        if (controls) {
-            setTimeout(function() {
+
+        if (controls && !this.debug.active) {
+            setTimeout(function () {
                 camera.controls.minPolarAngle = controls.minPolarAngle
                 camera.controls.maxPolarAngle = controls.maxPolarAngle
                 camera.controls.minAzimuthAngle = controls.minAzimuthAngle
                 camera.controls.maxAzimuthAngle = controls.maxAzimuthAngle
             }, this.data.moveDuration, controls)
         }
-        
+
         this.controls.autoRotate = false
     }
 
@@ -199,6 +173,8 @@ export default class Camera {
         TWEEN.update()
         this.controls.update()
 
+        this.lookAtLabel3D.setPosition(new THREE.Vector3(this.data.lookAt.x, this.data.lookAt.y, this.data.lookAt.z))
+
         if (this.controls.autoRotate) {
             this.changeRotateDirection()
         }
@@ -206,19 +182,8 @@ export default class Camera {
 
     addGUIControls() {
         const camera = this.debug.ui.addFolder('Camera')
-
-        this.debugCamera = new THREE.PerspectiveCamera(45, this.sizes.width / this.sizes.height, 0.01, 5)
-        this.debugCamera.position.set(0, 1.7, 3)
-        this.scene.add(this.debugCamera)
-
-        const cameraHelper = new THREE.CameraHelper(this.debugCamera)
-        this.scene.add(cameraHelper)
-
-
-        this.instance.position.copy(this.debugCamera.position)
-
-        // Location
         camera.close()
+
         camera
             .add(this.data, 'location')
             .min(0)
@@ -230,54 +195,8 @@ export default class Camera {
             .name('Location')
 
 
-        camera.add(this.data, 'debug').name('debug')
-            .onFinishChange((debug) => {
-                if (debug) {
-                    this.moveCameraTo(3)
-                } else {
-                    this.moveCameraTo(0)
-                }
-            })
-
-
         // Position
         const cameraPosition = camera.addFolder('Camera position')
 
-        cameraPosition
-            .add(this.data.position, 'x', -20, 20)
-            .name('X')
-            .onChange(
-                (value) => { this.instance.position.x = value, this.data.position.x = value })
-            .onFinishChange(
-                (value) => { console.log(value) }
-            )
-
-        cameraPosition
-            .add(this.data.position, 'y', -20, 20)
-            .name('Y')
-            .onChange((value) => { this.instance.position.y = value, this.data.position.y = value })
-
-        cameraPosition
-            .add(this.data.position, 'z', -20, 20)
-            .name('Z')
-            .onChange((value) => { this.instance.position.z = value, this.data.position.z = value })
-
-        // Lookat
-        const lookAtPosition = camera.addFolder('Look at point position')
-
-        lookAtPosition
-            .add(this.data.lookAt, 'x', -20, 20)
-            .name('X')
-            .onChange((value) => { this.instance.lookAt(new THREE.Vector3(value, 0, 0)), this.data.lookAt = new THREE.Vector3(value, 0, 0) })
-
-        lookAtPosition
-            .add(this.data.lookAt, 'y', -20, 20)
-            .name('Y')
-            .onChange((value) => { this.instance.lookAt(new THREE.Vector3(0, value, 0)), this.data.lookAt = new THREE.Vector3(0, value, 0) })
-
-        lookAtPosition
-            .add(this.data.lookAt, 'z', -20, 20)
-            .name('Z')
-            .onChange((value) => { this.instance.lookAt(new THREE.Vector3(0, 0, value)), this.data.lookAt = new THREE.Vector3(0, 0, value) })
     }
 }
