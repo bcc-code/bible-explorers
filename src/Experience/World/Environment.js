0bb8587b8338ec61a99abb4e06b3801d827237c5
Bogdan Niculae <bogdaniculae@gmail.com>
2022-04-04 18:22:04 +0300

    test lighting

--- src/Experience/World/Environment.js
+++ src/Experience/World/Environment.js
@@ -1,7 +1,5 @@
 import * as THREE from 'three'
 import Experience from '../Experience.js'
-import { RectAreaLightHelper } from 'three/examples/jsm/helpers/RectAreaLightHelper.js'
-import { RectAreaLightUniformsLib } from 'three/examples/jsm/lights/RectAreaLightUniformsLib.js'
 
 export default class Environment {
     constructor() {
@@ -10,21 +8,19 @@ export default class Environment {
         this.resources = this.experience.resources
         this.camera = this.experience.camera
         this.debug = this.experience.debug
+        this.renderer = this.experience.renderer
 
         // Setup
-        this.setSpotlight()
+        this.setPointLight()
+        this.setAmbientLight()
         this.setEnvironmentMap()
 
         if (this.debug.active) {
-            this.setAmbientLight()
-            this.setPlatformLight()
-            this.setElevatorLight()
 
             // Options
             this.data = {
-                colorPlatform: this.platformPointLight.color.getHex(),
-                colorElevator: this.elevatorPointLight.color.getHex(),
                 colorAmbient: this.ambientLight.color.getHex(),
+                // colorSpot: this.spotLight.color.getHex(),
                 mapsEnabled: true,
             }
 
@@ -32,40 +28,40 @@ export default class Environment {
         }
     }
 
-    setPlatformLight() {
-        this.platformPointLight = new THREE.PointLight(0xffaf8c, 50, 10);
-        this.platformPointLight.position.set(0, 3.75, 0);
-        this.platformPointLight.name = 'Platform_light'
-        this.platformPointLight.castShadow = true
-        this.platformPointLight.shadow.camera.far = 5
-        this.platformPointLight.shadow.camera.fov = 60
-        this.scene.add(this.platformPointLight);
+    setPointLight() {
+        this.platformLight = new THREE.PointLight(0xffffff, 1, 100);
+        this.platformLight.position.set(0, 3.5, 0);
+        this.platformLight.decay = 2
+        this.platformLight.power = 800
+        this.scene.add(this.platformLight);
     }
-    setElevatorLight() {
-        this.elevatorPointLight = new THREE.PointLight(0xffaf8c, 50, 10);
-        this.elevatorPointLight.position.set(-11.3, 3.6, 0);
-        this.elevatorPointLight.name = 'Elevator_light'
-        this.elevatorPointLight.castShadow = true
-        this.elevatorPointLight.shadow.camera.far = 5
-        this.elevatorPointLight.shadow.camera.fov = 60
 
-        this.scene.add(this.elevatorPointLight);
+    setSpotlight() {
+        this.spotLight = new THREE.SpotLight(0xffaf8c);
+
+        this.spotLight.castShadow = true
+
+        this.spotLight.shadow.mapSize.width = 1024;
+        this.spotLight.shadow.mapSize.height = 1024;
+
+        const targetObject = new THREE.Object3D()
+        this.scene.add(targetObject)
+
+        this.spotLight.target = targetObject
+
+        this.spotLight.power = 200
+        this.spotLight.distance = 10
+        this.spotLight.angle = Math.PI / 4
+        this.spotLight.decay = 2
+        this.spotLight.penumbra = 1
+        this.spotLight.position.set(0, 3.5, 0);
+
+        this.scene.add(this.spotLight);
     }
 
     setAmbientLight() {
-        this.ambientLight = new THREE.AmbientLight(0xfcafff, 1);
+        this.ambientLight = new THREE.AmbientLight(0xffffff, 1);
         this.scene.add(this.ambientLight);
-    }
-
-    setVideoLight() {
-        RectAreaLightUniformsLib.init();
-
-        this.videoLight = new THREE.RectAreaLight(0xffffff, 20, 16.4, 9.2)
-        this.videoLight.position.set(17.1, 3, 0)
-        this.videoLight.rotation.y = THREE.MathUtils.degToRad(90)
-        this.videoLight.name = 'Video Light'
-        this.videoLight.lookAt(0, 0, 0)
-        this.scene.add(new RectAreaLightHelper(this.videoLight))
     }
 
     setEnvironmentMap() {
@@ -91,87 +87,49 @@ export default class Environment {
         this.environmentMap.updateMaterials()
     }
 
-    setSpotlight() {
-        const spotLight = new THREE.SpotLight(0xffaf8c);
-        spotLight.power = 800
-        spotLight.angle = Math.PI
-        spotLight.decay = 2
-        spotLight.penumbra = 1
-        spotLight.position.set(0, 3.5, 0);
-        this.scene.add(spotLight);
-
-        const spotLightHelper = new THREE.SpotLightHelper(spotLight);
-        this.scene.add(spotLightHelper);
-    }
 
     addGUIControls() {
         if (this.debug.active) {
-
-            const bulbGeometry = new THREE.SphereGeometry(0.2, 16, 8)
-            const bulbMaterial = new THREE.MeshStandardMaterial({
-                emissive: 0xffaf8c,
-                emissiveIntensity: this.platformPointLight.intensity,
-                color: 0x000000
-            })
-            const platformBulb = new THREE.Mesh(bulbGeometry, bulbMaterial)
-            const elevatorBulb = new THREE.Mesh(bulbGeometry, bulbMaterial)
-
-            // this.platformPointLight.add(platformBulb)
-            // this.elevatorPointLight.add(elevatorBulb)
 
             const environment = this.debug.ui.addFolder('Environment')
             environment.close()
             environment.add(this.environmentMap, 'intensity').min(0).max(20).step(0.01).name('intensity').onChange(() => { this.environmentMap.updateMaterials() })
 
             const light = this.debug.ui.addFolder('Light')
-            light.close()
+            // light.close()
 
             const ambientLight = light.addFolder('Ambient light')
-            ambientLight.close()
+            // ambientLight.close()
             ambientLight.addColor(this.data, 'colorAmbient').onChange(() => {
                 this.ambientLight.color.setHex(Number(this.data.colorAmbient.toString().replace('#', '0x')))
             })
             ambientLight.add(this.ambientLight, 'intensity').min(0).max(100).step(0.01).name('intensity')
 
+            const sphereSize = 1;
+            const pointLightHelper = new THREE.PointLightHelper(this.platformLight, sphereSize);
+            this.scene.add(pointLightHelper);
 
-            const platformLight = light.addFolder('Platform light')
-            platformLight.close()
+            if (!this.spotLight) return
 
-            platformLight.addColor(this.data, 'colorPlatform').onChange(() => {
-                this.platformPointLight.color.setHex(Number(this.data.colorPlatform.toString().replace('#', '0x')))
+            this.spotLightHelper = new THREE.SpotLightHelper(this.spotLight);
+            this.scene.add(this.spotLightHelper);
+
+            const spotLight = light.addFolder('Spotlight Platform')
+            spotLight.addColor(this.data, 'colorSpot').onChange(() => {
+                this.spotLight.color.setHex(Number(this.data.colorSpot.toString().replace('#', '0x')))
             })
-            platformLight.add(this.platformPointLight, 'distance').min(0).max(20).step(0.01).name('distance')
-            platformLight.add(this.platformPointLight, 'decay').min(0).max(4).step(0.1).name('decay')
-            platformLight.add(this.platformPointLight, 'intensity').min(0).max(100).step(0.01).name('intensity')
-            platformLight.add(this.platformPointLight.position, 'x').min(-20).max(20).step(0.01).name('lightX')
-            platformLight.add(this.platformPointLight.position, 'y').min(-20).max(20).step(0.01).name('lightY')
-            platformLight.add(this.platformPointLight.position, 'z').min(-20).max(20).step(0.01).name('lightZ')
 
-            this.platformLightHelper = new THREE.CameraHelper(this.platformPointLight.shadow.camera)
-            this.platformLightHelper.visible = false
-            this.scene.add(this.platformLightHelper)
+            spotLight.add(this.spotLight.position, 'x', -20, 20).step(0.01).onChange((val) => { this.spotLight.position.x = val })
+            spotLight.add(this.spotLight.position, 'y', -10, 10).step(0.01).onChange((val) => { this.spotLight.position.y = val })
+            spotLight.add(this.spotLight.position, 'z', -20, 20).step(0.01).onChange((val) => { this.spotLight.position.z = val })
 
-            platformLight.add(this.platformLightHelper, 'visible').name('light helper')
-
-            const elevatorLight = light.addFolder('Elevator light')
-            elevatorLight.close()
-
-            elevatorLight.addColor(this.data, 'colorElevator').onChange(() => {
-                this.elevatorPointLight.color.setHex(Number(this.data.colorElevator.toString().replace('#', '0x')))
-            })
-            elevatorLight.add(this.elevatorPointLight, 'distance').min(0).max(20).step(0.01).name('distance')
-            elevatorLight.add(this.elevatorPointLight, 'decay').min(0).max(4).step(0.1).name('decay')
-            elevatorLight.add(this.elevatorPointLight, 'intensity').min(0).max(100).step(0.01).name('intensity')
-            elevatorLight.add(this.elevatorPointLight.position, 'x').min(-20).max(20).step(0.01).name('lightX')
-            elevatorLight.add(this.elevatorPointLight.position, 'y').min(-20).max(20).step(0.01).name('lightY')
-            elevatorLight.add(this.elevatorPointLight.position, 'z').min(-20).max(20).step(0.01).name('lightZ')
-
-            this.elevatorLightHelper = new THREE.CameraHelper(this.elevatorPointLight.shadow.camera)
-            this.elevatorLightHelper.visible = false
-            this.scene.add(this.elevatorLightHelper)
-
-            elevatorLight.add(this.elevatorLightHelper, 'visible').name('light helper')
+            spotLight.add(this.spotLight, 'intensity', 0, 2).onChange((val) => { this.spotLight.intensity = val, this.spotLightHelper.update() })
+            spotLight.add(this.spotLight, 'penumbra', 0, 1).onChange((val) => { this.spotLight.penumbra = val, this.spotLightHelper.update() })
+            spotLight.add(this.spotLight, 'distance', 0, 20).step(0.1).onChange((val) => { this.spotLight.distance = val, this.spotLightHelper.update() })
+            spotLight.add(this.spotLight, 'angle', 0, Math.PI / 2).onChange((val) => { this.spotLight.angle = val, this.spotLightHelper.update() })
+            spotLight.add(this.spotLight, 'decay', 1, 2).onChange((val) => { this.spotLight.decay = val, this.spotLightHelper.update() })
 
         }
     }
+
 }
