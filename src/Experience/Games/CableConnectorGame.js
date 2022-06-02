4b0615b4aa724c3f193720b5082a778de6d6812e
Bogdan Niculae <bogdaniculae@gmail.com>
2022-06-02 19:00:03 +0300

    Update CableConnectorGame.js

--- src/Experience/Games/CableConnectorGame.js
+++ src/Experience/Games/CableConnectorGame.js
@@ -331,7 +331,7 @@ export default class CableConnector {
                 outlet.item.shadowBlur(0)
             })
 
-            outlet.item.on('click', () => {
+            outlet.item.on('touchstart click', () => {
                 if (!outlet.canClick) return
                 if (outlet.connected) return
 
