44774b7cc4d8ef2db9f324d5cfd919bca6535622
Bogdan Niculae <bogdaniculae@gmail.com>
2023-01-30 18:08:18 +0200

    add env map again

--- src/Experience/World/Environment.js
+++ src/Experience/World/Environment.js
@@ -11,7 +11,7 @@ export default class Environment {
         this.renderer = this.experience.renderer
 
         // Setup
-        // this.setEnvironmentMap()
+        this.setEnvironmentMap()
 
     }
 
