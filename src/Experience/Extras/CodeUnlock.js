21a25a06d65e73aecaa06b86e40ff5790f11b532
Bogdan Niculae <bogdaniculae@gmail.com>
2023-02-02 18:41:56 +0200

    code unlock new UI and logic

--- src/Experience/Extras/CodeUnlock.js
+++ src/Experience/Extras/CodeUnlock.js
@@ -1,6 +1,7 @@
-import Experience from "../Experience.js";
+import Experience from "../Experience.js"
 import Modal from '../Utils/Modal.js'
 import _s from '../Utils/Strings.js'
+import _gl from '../Utils/Globals.js'
 
 let instance = null
 const showSkipAfterNoOfTries = 3
@@ -12,140 +13,207 @@ export default class CodeUnlock {
         this.debug = this.experience.debug
 
         instance = this
-        instance.fails = 0
+
     }
 
     toggleCodeUnlock() {
-        if (document.querySelector('.modal')) {
-            instance.modal.destroy()
+
+        instance.audio = instance.world.audio
+        instance.program = instance.world.program
+        instance.secretCode = instance.program.getCurrentStepData().code_to_unlock
+
+        instance.data = {
+            codeLength: 5,
+            fails: 0,
+            currentNumberIndex: 0,
+            enteredCode: [],
         }
-        else {
-            instance.audio = instance.world.audio
-            instance.program = instance.world.program
-            instance.secretCode = instance.program.getCurrentStepData().code_to_unlock
 
-            let html = `
-                <div class="modal__content code-unlock">
-                    <div class="code-unlock__container">
-                        <div class="code-unlock__screen">
-                            <span class="code-unlock__input"></span>
-                        </div>
-                        <div class="code-unlock__grid">
-                            <button type="button" class="code-unlock__btn code-unlock__btn--number">7</button>
-                            <button type="button" class="code-unlock__btn code-unlock__btn--number">8</button>
-                            <button type="button" class="code-unlock__btn code-unlock__btn--number">9</button>
-                            <button type="button" class="code-unlock__btn code-unlock__btn--number">4</button>
-                            <button type="button" class="code-unlock__btn code-unlock__btn--number">5</button>
-                            <button type="button" class="code-unlock__btn code-unlock__btn--number">6</button>
-                            <button type="button" class="code-unlock__btn code-unlock__btn--number">1</button>
-                            <button type="button" class="code-unlock__btn code-unlock__btn--number">2</button>
-                            <button type="button" class="code-unlock__btn code-unlock__btn--number">3</button>
-                            <button type="button" class="code-unlock__btn code-unlock__btn--backspace"><i class="icon icon-arrow-left-long-to-line-solid"></i></button>
-                            <button type="button" class="code-unlock__btn code-unlock__btn--number">0</button>
-                            <button type="button" class="code-unlock__btn code-unlock__btn--confirm"><i class="icon icon-check-solid"></i></button>
-                        </div>
-                    </div>
-                </div>`
-
-            instance.modal = new Modal(html, 'modal__code-unlock')
-
-            const title = document.querySelector('.modal__heading--minigame')
-            title.innerHTML = `<h3>${_s.task.codeUnlock}</h3>`
-
-            const back = document.getElementById("back")
-            back.style.display = 'block'
-            back.innerText = _s.journey.back
-            back.addEventListener('click', (e) => {
-                instance.modal.destroy()
-                instance.program.previousStep()
-            })
-
-            const skip = document.getElementById("skip")
-            skip.innerText = _s.miniGames.skip
-            skip.style.display = instance.debug.developer || instance.debug.onQuickLook() || instance.fails >= showSkipAfterNoOfTries
-                ? 'block'
-                : 'none'
-            skip.addEventListener('click', instance.finishGame)
-
-            instance.el = {
-                code: document.querySelector(".code-unlock__input"),
-                numbers: document.querySelectorAll(".code-unlock__btn--number"),
-                backspace: document.querySelector(".code-unlock__btn--backspace"),
-                confirm: document.querySelector(".code-unlock__btn--confirm")
-            }
-
-            instance.el.numbers.forEach(function (number) {
-                number.addEventListener("click", () => {
-                    instance.add(number.textContent)
-                })
-            })
-
-            instance.el.backspace.addEventListener("click", instance.remove)
-            instance.el.confirm.addEventListener("click", instance.checkCode)
-
-            document.onkeydown = (e) => {
-                if (e.key === '1' ||
-                    e.key === '2' ||
-                    e.key === '3' ||
-                    e.key === '4' ||
-                    e.key === '5' ||
-                    e.key === '6' ||
-                    e.key === '7' ||
-                    e.key === '8' ||
-                    e.key === '9' ||
-                    e.key === '0'
-                ) {
-                    instance.add(e.key)
-                }
-                else if (e.key === 'Backspace') {
-                    instance.remove()
-                }
-                else if (e.key === 'Enter') {
-                    instance.checkCode()
-                }
-            }
-        }
+        instance.unlockScreenHTML()
     }
 
-    add(number) {
-        const maxLength = 10
-        if (instance.el.code.textContent.length == maxLength) return
-        instance.el.code.textContent += number
+    unlockScreenHTML() {
+
+        const unlockScreen = _gl.elementFromHtml(`
+            <section class="code-unlock">
+                <div class="container">
+                    <div class="code-unlock-device">
+                        <div class="code-unlock-code"></div>
+                        <div class="code-unlock-keyboard"></div>
+                    </div>
+                </div>
+                <div class="overlay"></div>
+            </section>
+        `)
+
+        for (let i = 1; i <= 12; i++) {
+
+            if (i == 10) {
+                const deleteKey = _gl.elementFromHtml(`
+                    <button class="code-unlock-key delete-number">
+                        <svg class="delete-icon icon" viewBox="0 0 27 18">
+                            <use href="#delete-left"></use>
+                        </svg>
+                    </button>
+                `)
+                deleteKey.disabled = true
+                unlockScreen.querySelector('.code-unlock-keyboard').append(deleteKey)
+            }
+
+            else if (i == 11) {
+                const key = _gl.elementFromHtml(`<button class="code-unlock-key">0</button>`)
+                unlockScreen.querySelector('.code-unlock-keyboard').append(key)
+            }
+
+            else if (i == 12) {
+                const confirmKey = _gl.elementFromHtml(`
+                    <button class="code-unlock-key confirm-code">
+                        <svg class="confirm-icon icon" viewBox="0 0 23 16">
+                            <use href="#check-mark"></use>
+                        </svg>
+                    </button>
+                `)
+
+                confirmKey.disabled = true
+                unlockScreen.querySelector('.code-unlock-keyboard').append(confirmKey)
+
+            }
+
+            else {
+                const key = _gl.elementFromHtml(`<button class="code-unlock-key">${i}</button>`)
+                unlockScreen.querySelector('.code-unlock-keyboard').append(key)
+            }
+        }
+
+        for (let j = 0; j < instance.secretCode.length; j++) {
+            const asterix = _gl.elementFromHtml(`
+                <div>
+                    <svg class="asterisk-icon icon" width="20" height="22" viewBox="0 0 20 22">
+                        <use href="#asterisk"></use>
+                    </svg>
+                </div>
+            `)
+            unlockScreen.querySelector('.code-unlock-code').append(asterix)
+        }
+
+        document.querySelector('.ui-container').append(unlockScreen)
+
+        instance.el = {
+            code: unlockScreen.querySelector(".code-unlock-code"),
+            numbers: unlockScreen.querySelectorAll(".code-unlock-key"),
+            backspace: document.querySelector('.delete-number'),
+            confirm: document.querySelector('.confirm-code')
+        }
+
+        instance.el.numbers.forEach(function (number) {
+
+            number.addEventListener("click", () => {
+
+                if (number.matches('.delete-number') || number.matches('.confirm-code')) return
+                instance.add(number.textContent)
+            })
+        })
+
+        instance.el.backspace.addEventListener("click", instance.remove)
+        instance.el.confirm.addEventListener("click", instance.checkCode)
+
+        instance.experience.navigation.next.addEventListener('click', instance.destroy)
+        instance.experience.navigation.prev.addEventListener('click', instance.destroy)
+
+        document.onkeydown = (e) => {
+            if (e.key === '1' ||
+                e.key === '2' ||
+                e.key === '3' ||
+                e.key === '4' ||
+                e.key === '5' ||
+                e.key === '6' ||
+                e.key === '7' ||
+                e.key === '8' ||
+                e.key === '9' ||
+                e.key === '0'
+            ) {
+                instance.add(e.key)
+            }
+            else if (e.key === 'Backspace') {
+                if (instance.data.currentNumberIndex > 0)
+                    instance.remove()
+            }
+            else if (e.key === 'Enter') {
+                if (instance.data.currentNumberIndex == instance.data.codeLength)
+                    instance.checkCode()
+            }
+        }
+
+    }
+
+    add(num) {
+        if (instance.data.currentNumberIndex == instance.data.codeLength) return
+
+        if (instance.data.currentNumberIndex == instance.data.codeLength - 1)
+            instance.el.confirm.disabled = false
+
+        if (instance.data.currentNumberIndex == 1)
+            instance.el.backspace.disabled = false
+
+        const numbers = instance.el.code.querySelectorAll('div')
+        numbers[instance.data.currentNumberIndex].textContent = num
+        instance.data.enteredCode.push(num)
+        instance.data.currentNumberIndex++
     }
 
     remove() {
-        if (instance.el.code.textContent == "") return
-        instance.el.code.textContent = instance.el.code.textContent.slice(0, -1)
+
+        if (instance.data.currentNumberIndex == 0) return
+
+        document.querySelector('.code-unlock.correct-code')?.classList.remove('correct-code')
+
+        const asterisk = _gl.elementFromHtml(`
+                <svg class="asterisk-icon icon" width="20" height="22" viewBox="0 0 20 22">
+                    <use href="#asterisk"></use>
+                </svg>
+            `)
+
+        const numbers = instance.el.code.querySelectorAll('div')
+        numbers[instance.data.currentNumberIndex - 1].textContent = ''
+        numbers[instance.data.currentNumberIndex - 1].appendChild(asterisk)
+
+
+        if (instance.data.currentNumberIndex == 1)
+            instance.el.backspace.disabled = true
+
+        instance.data.enteredCode.splice(instance.data.currentNumberIndex - 1, 1)
+        instance.data.currentNumberIndex--
+
+        if (instance.data.currentNumberIndex != instance.data.codeLength)
+            instance.el.confirm.disabled = true
     }
 
     checkCode() {
         const wrapper = document.querySelector('.code-unlock')
 
-        if (instance.el.code.textContent == instance.secretCode) {
+        if (instance.data.enteredCode.join('') == instance.secretCode) {
+            wrapper.classList.add('correct-code')
             instance.audio.playSound('task-completed')
-            instance.finishGame()
+            instance.data.fails = 0
         }
         else {
-            instance.fails++
-            if (instance.fails >= showSkipAfterNoOfTries)
-                document.getElementById("skip").style.display = 'block'
+            instance.data.fails++
+            // if (instance.fails >= showSkipAfterNoOfTries)
+            //     document.getElementById("skip").style.display = 'block'
 
             wrapper.classList.add('wrong-code')
             setTimeout(() => {
                 wrapper.classList.remove('wrong-code')
-            }, 2000)
+            }, 500)
             instance.audio.playSound('wrong')
         }
     }
 
-    finishGame() {
-        instance.fails = 0
-        instance.destroy()
-        instance.program.nextStep()
-    }
-
     destroy() {
-        instance.modal.destroy()
         document.onkeydown = null
+
+        document.querySelector('.code-unlock')?.remove()
+        instance.experience.navigation.next.removeEventListener('click', instance.destroy)
+        instance.experience.navigation.prev.removeEventListener('click', instance.destroy)
     }
 }
