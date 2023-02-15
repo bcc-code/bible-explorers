3515155c75686e10861d3288b4aaf19f8d40e0d7
Simon Costea <simoncostea@yahoo.com>
2023-02-15 14:16:37 +0200

    Fix congrats event listeners

--- src/Experience/Extras/Congrats.js
+++ src/Experience/Extras/Congrats.js
@@ -1,5 +1,4 @@
 import Experience from "../Experience.js"
-import Modal from '../Utils/Modal.js'
 import _s from '../Utils/Strings.js'
 import _gl from '../Utils/Globals.js'
 
@@ -7,16 +6,37 @@ let instance = null
 
 export default class Congrats {
     constructor() {
-        this.experience = new Experience()
-        this.world = this.experience.world
-        this.camera = this.experience.camera
-        this.debug = this.experience.debug
-
         instance = this
+        instance.experience = new Experience()
+        instance.world = instance.experience.world
+    }
+
+    toggleSummary() {
+        instance.destroy()
+        instance.experience.navigation.prev.addEventListener('click', instance.destroy)
+        instance.world.audio.playSound('task-completed')
+
+        const summary = _gl.elementFromHtml(`
+            <div class="modal">
+                <div class="container">
+                    <div class="summary">
+                        <header>
+                            <h1>${_s.miniGames.completed.title}</h1>
+                        </header>
+                    </div>
+                </div>
+                <div class="overlay"></div>
+            </div>
+        `)
+
+        document.querySelector('.ui-container').append(summary)
     }
 
     toggleBibleCardsReminder() {
         instance.destroy()
+        instance.world.program.destroy()
+
+        instance.experience.navigation.next.addEventListener('click', instance.toggleCongrats)
 
         const bibleCards = _gl.elementFromHtml(`
             <div class="modal">
@@ -33,15 +53,11 @@ export default class Congrats {
         `)
 
         document.querySelector('.ui-container').append(bibleCards)
-
-        instance.experience.navigation.next.addEventListener('click', () => {
-            instance.destroy()
-            instance.toggleCongrats()
-        })
     }
 
     toggleCongrats() {
         instance.destroy()
+        instance.experience.navigation.next.addEventListener('click', instance.finishChapter)
         instance.world.audio.playSound('congrats')
 
         const chapterCongrats = _gl.elementFromHtml(`
@@ -59,35 +75,11 @@ export default class Congrats {
         `)
 
         document.querySelector('.ui-container').append(chapterCongrats)
-
-        instance.experience.navigation.next.addEventListener('click', () => {
-            instance.destroy()
-            instance.world.goHome()
-            instance.debug.removeQuickLookMode()
-
-            if (!instance.experience.settings.fullScreen && document.fullscreenElement) {
-                document.exitFullscreen()
-            }
-        })
     }
 
-    toggleSummary() {
+    finishChapter() {
         instance.destroy()
-        instance.world.audio.playSound('task-completed')
-        const summary = _gl.elementFromHtml(`
-            <div class="modal">
-                <div class="container">
-                    <div class="summary">
-                        <header>
-                            <h1>${_s.miniGames.completed.title}</h1>
-                        </header>
-                    </div>
-                </div>
-                <div class="overlay"></div>
-            </div>
-        `)
-
-        document.querySelector('.ui-container').append(summary)
+        instance.world.goHome()
     }
 
     animateStars(timeout) {
@@ -101,7 +93,9 @@ export default class Congrats {
 
     destroy() {
         document.querySelector('.modal')?.remove()
-        instance.experience.navigation.next.removeEventListener('click', instance.destroy)
         instance.experience.navigation.prev.removeEventListener('click', instance.destroy)
+        instance.experience.navigation.next.removeEventListener('click', instance.destroy)
+        instance.experience.navigation.next.removeEventListener('click', instance.toggleCongrats)
+        instance.experience.navigation.next.removeEventListener('click', instance.finishChapter)
     }
 }
