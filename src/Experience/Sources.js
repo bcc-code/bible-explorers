319d0f207da0ff5f77b57872a9283bb70308af89
Bogdan Niculae <bogdaniculae@gmail.com>
2023-04-24 17:29:30 +0300

    config vite dev server

--- src/Experience/Sources.js
+++ src/Experience/Sources.js
@@ -14,7 +14,7 @@ export default [
     {
         name: 'controlRoom',
         type: 'gltfModel',
-        path: 'models/Controlroom_lowmodel_1.glb'
+        path: 'models/Controlroom.glb'
     },
     {
         name: 'baked',
