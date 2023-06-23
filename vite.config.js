70daeb6c54c013d1c98afafb077247a819b38968
Simon Costea <simoncostea@yahoo.com>
2023-06-23 18:40:32 +0300

    Workbox runtime caching

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,20 @@
 const isCodeSandbox = 'SANDBOX_URL' in process.env || 'CODESANDBOX_HOST' in process.env
 
+const getCache = ({ name, pattern }) => ({
+    urlPattern: pattern,
+    handler: "NetworkFirst",
+    options: {
+      cacheName: name,
+      expiration: {
+        maxEntries: 500,
+        maxAgeSeconds: 60 * 60 * 24 * 365 * 2 // 2 years
+      },
+      cacheableResponse: {
+        statuses: [0, 200]
+      }
+    }
+})
+
 import basicSsl from '@vitejs/plugin-basic-ssl'
 import { VitePWA } from 'vite-plugin-pwa'
 
@@ -23,14 +38,27 @@ export default {
     plugins: [
         basicSsl(),
         VitePWA({
-            injectRegister: 'null',
             registerType: 'autoUpdate',
             includeAssets: ['*'],
+            injectRegister: 'null',
             injectManifest: {
                 swSrc: './src/js/sw/sw.js',
                 swDest: './sw.js',
                 maximumFileSizeToCacheInBytes: 20000000,
                 globPatterns: ['**/*.{js,css,html,png,svg,mp3,mp4}']
+            },
+            workbox: {
+                maximumFileSizeToCacheInBytes: 20000000,
+                runtimeCaching: [
+                    getCache({ 
+                        pattern: "https://www.gstatic.com/cast/sdk/libs/sender/1.0/cast_framework.js", 
+                        name: "castFramework" 
+                    }),
+                    getCache({ 
+                        pattern: "https://unpkg.com/@rive-app/canvas@1.0.102", 
+                        name: "riveApp" 
+                    })
+                ]
             }
         })
     ]
