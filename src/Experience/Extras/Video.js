6f23ea52fab3846b0d8db1111957b737b96c90d7
Bogdan Niculae <bogdaniculae@gmail.com>
2023-09-12 15:55:49 +0300

    Update Video.js
    remove tablet play/pause - not used anymore

--- src/Experience/Extras/Video.js
+++ src/Experience/Extras/Video.js
@@ -1,261 +1,268 @@
-import * as THREE from 'three'
-import TWEEN from '@tweenjs/tween.js'
-import Experience from "../Experience.js";
-import _lang from '../Utils/Lang.js'
-import _s from '../Utils/Strings.js'
-import _e from '../Utils/Events.js'
+import * as THREE from 'three';
+import TWEEN from '@tweenjs/tween.js';
+import Experience from '../Experience.js';
+import _lang from '../Utils/Lang.js';
+import _s from '../Utils/Strings.js';
+import _e from '../Utils/Events.js';
 
-let instance = null
+let instance = null;
 
 export default class Video {
-    constructor() {
-        if (instance)
-            return instance
+  constructor() {
+    if (instance) return instance;
 
-        instance = this
+    instance = this;
 
-        instance.experience = new Experience()
-        instance.world = instance.experience.world
-        instance.debug = instance.experience.debug
-        instance.resources = instance.experience.resources
-        instance.camera = instance.experience.camera
-        instance.audio = instance.world.audio
-        instance.scene = instance.experience.scene
-        instance.controlRoom = instance.world.controlRoom
-        instance.clickableObjects = instance.controlRoom.clickableObjects
+    instance.experience = new Experience();
+    instance.world = instance.experience.world;
+    instance.debug = instance.experience.debug;
+    instance.resources = instance.experience.resources;
+    instance.camera = instance.experience.camera;
+    instance.audio = instance.world.audio;
+    instance.scene = instance.experience.scene;
+    instance.controlRoom = instance.world.controlRoom;
+    instance.clickableObjects = instance.controlRoom.clickableObjects;
 
-        // Setup
-        instance.portalScreen = instance.controlRoom.tv_portal
-        instance.tablet = instance.controlRoom.tablet
-        instance.videoPlayIcon = null
+    // Setup
+    instance.portalScreen = instance.controlRoom.tv_portal;
+    instance.tablet = instance.controlRoom.tablet;
+    instance.videoPlayIcon = null;
 
-        instance.canvasTexture()
+    instance.canvasTexture();
 
-        instance.video = () => {
-            let id = instance.playingVideoId
-            return instance.resources.videoPlayers[id]
-        }
+    instance.video = () => {
+      let id = instance.playingVideoId;
+      return instance.resources.videoPlayers[id];
+    };
 
-        instance.videoJsEl = () => {
-            let id = 'videojs-' + instance.playingVideoId
-            return document.getElementById(id)
-        }
+    instance.videoJsEl = () => {
+      let id = 'videojs-' + instance.playingVideoId;
+      return document.getElementById(id);
+    };
 
-        instance.hasSkipBtn = () => {
-            return instance.videoJsEl().querySelector('.skip-video') != null
-        }
+    instance.hasSkipBtn = () => {
+      return instance.videoJsEl().querySelector('.skip-video') != null;
+    };
 
-        instance.getSkipBtn = () => {
-            return instance.videoJsEl().querySelector('.skip-video')
-        }
+    instance.getSkipBtn = () => {
+      return instance.videoJsEl().querySelector('.skip-video');
+    };
 
-        instance.playingVideoId = null
+    instance.playingVideoId = null;
+  }
+
+  canvasTexture() {
+    // create image
+    const bitmap = createRetinaCanvas(1920, 1080, 1);
+    const ctx = bitmap.getContext('2d', { antialias: false });
+
+    const centerX = bitmap.width / 2;
+    const centerY = bitmap.height / 2;
+    const size = 40;
+    const circle = size * 2.5;
+
+    ctx.globalAlpha = 0.4;
+    ctx.beginPath();
+    ctx.rect(0, 0, 1920, 1080);
+    ctx.fillStyle = 'black';
+    ctx.fill();
+
+    ctx.globalAlpha = 1.0;
+    ctx.lineWidth = 20;
+    ctx.strokeStyle = '#ffffff';
+    ctx.fillStyle = '#ffffff';
+
+    // make circle
+    ctx.beginPath();
+    ctx.arc(centerX, centerY, circle, 0, 2 * Math.PI);
+    ctx.closePath();
+    ctx.stroke();
+
+    // make play button
+    ctx.lineJoin = 'round';
+
+    ctx.beginPath();
+    ctx.moveTo(centerX - size + 10, centerY - size);
+    ctx.lineTo(centerX - size + 10, centerY + size);
+    ctx.lineTo(centerX + size + 5, centerY);
+    ctx.closePath();
+    ctx.stroke();
+    ctx.fill();
+
+    // canvas contents are used for
+    const texture = new THREE.Texture(bitmap);
+    texture.needsUpdate = true;
+
+    const material = new THREE.MeshBasicMaterial({
+      color: '#ffffff',
+      map: texture,
+      transparent: true,
+    });
+
+    const geometry = new THREE.PlaneGeometry(16, 9);
+    const mesh = new THREE.Mesh(geometry, material);
+    mesh.name = 'play_video_icon';
+    this.scene.add(mesh);
+
+    mesh.position.copy(instance.portalScreen.position);
+    mesh.quaternion.copy(instance.portalScreen.quaternion);
+    mesh.position.x -= 0.01;
+    mesh.visible = false;
+
+    this.videoPlayIcon = mesh;
+    this.clickableObjects.push(mesh);
+  }
+
+  load(id) {
+    instance.playingVideoId = id;
+
+    // First, remove all previous event listeners - if any
+    instance.video().off('ended', instance.waitAndFinish);
+    instance.video().off('play', instance.setFullscreenIfNecessary);
+    instance.video().off('fullscreenchange', instance.pauseOnFullscreenExit);
+
+    // Always start new loaded videos from the beginning
+    instance.video().currentTime(0);
+
+    // Set texture when starting directly on a video task type
+    if (
+      instance.portalScreen.material.map !=
+      instance.resources.customTextureItems[id]
+    )
+      instance.setTexture(id);
+
+    const videoQuality = instance.getVideoQuality();
+    instance.resources.videoPlayers[id].setVideoQuality(videoQuality);
+
+    instance.video().on('play', instance.setFullscreenIfNecessary);
+    instance.video().on('fullscreenchange', instance.pauseOnFullscreenExit);
+    instance.video().on('ended', instance.waitAndFinish);
+
+    instance.focus();
+    instance.addSkipBtn();
+  }
+
+  setTexture(id) {
+    if (!instance.resources.customTextureItems.hasOwnProperty(id)) return;
+
+    instance.portalScreen.material.map =
+      instance.resources.customTextureItems[id];
+    instance.portalScreen.material.map.flipY = false;
+    instance.portalScreen.material.needsUpdate = true;
+  }
+
+  //#region Actions
+
+  play() {
+    instance.video().play();
+    instance.experience.videoIsPlaying = true;
+  }
+
+  pause() {
+    instance.video().pause();
+    instance.experience.videoIsPlaying = false;
+  }
+
+  focus() {
+    instance.camera.zoomIn(2000);
+
+    instance.audio.setOtherAudioIsPlaying(true);
+    instance.audio.fadeOutBgMusic();
+
+    new TWEEN.Tween(instance.portalScreen.material)
+      .to({ color: new THREE.Color(0xffffff) }, 1000)
+      .easing(TWEEN.Easing.Quadratic.InOut)
+      .start();
+  }
+
+  defocus() {
+    if (instance.video()) {
+      instance.pause();
+      instance.portalScreen.scale.set(0, 0, 0);
+      instance.videoPlayIcon.visible = false;
+
+      if (instance.video().isFullscreen_) instance.video().exitFullscreen();
+
+      instance.audio.setOtherAudioIsPlaying(false);
+      instance.audio.fadeInBgMusic();
     }
 
-    canvasTexture() {
-        // create image
-        const bitmap = createRetinaCanvas(1920, 1080, 1)
-        const ctx = bitmap.getContext('2d', { antialias: false })
+    // instance.experience.navigation.next.disabled = false
+  }
 
-        const centerX = bitmap.width / 2
-        const centerY = bitmap.height / 2
-        const size = 40
-        const circle = size * 2.5
+  waitAndFinish() {
+    setTimeout(() => {
+      instance.finish();
+    }, 1000);
+  }
 
-        ctx.globalAlpha = 0.4
-        ctx.beginPath()
-        ctx.rect(0, 0, 1920, 1080)
-        ctx.fillStyle = 'black'
-        ctx.fill()
+  finish() {
+    if (instance.hasSkipBtn()) instance.getSkipBtn().remove();
 
-        ctx.globalAlpha = 1.0;
-        ctx.lineWidth = 20
-        ctx.strokeStyle = "#ffffff"
-        ctx.fillStyle = "#ffffff"
+    instance.defocus();
+    instance.world.program.nextStep();
+    setTimeout(() => {
+      instance.playingVideoId = null;
+    }, 1000);
+  }
 
-        // make circle
-        ctx.beginPath()
-        ctx.arc(centerX, centerY, circle, 0, 2 * Math.PI)
-        ctx.closePath()
-        ctx.stroke()
+  //#endregion
 
-        // make play button
-        ctx.lineJoin = "round"
+  getVideoQuality() {
+    switch (instance.world.selectedQuality) {
+      case 'low':
+        return 270;
 
-        ctx.beginPath()
-        ctx.moveTo(centerX - size + 10, centerY - size)
-        ctx.lineTo(centerX - size + 10, centerY + size)
-        ctx.lineTo(centerX + size + 5, centerY)
-        ctx.closePath()
-        ctx.stroke()
-        ctx.fill()
+      case 'medium':
+        return 540;
 
-        // canvas contents are used for
-        const texture = new THREE.Texture(bitmap)
-        texture.needsUpdate = true
-
-        const material = new THREE.MeshBasicMaterial({ color: '#ffffff', map: texture, transparent: true })
-
-        const geometry = new THREE.PlaneGeometry(16, 9)
-        const mesh = new THREE.Mesh(geometry, material)
-        mesh.name = 'play_video_icon'
-        this.scene.add(mesh)
-
-        mesh.position.copy(instance.portalScreen.position)
-        mesh.quaternion.copy(instance.portalScreen.quaternion)
-        mesh.position.x -= 0.01
-        mesh.visible = false
-
-        this.videoPlayIcon = mesh
-        this.clickableObjects.push(mesh)
+      case 'high':
+      default:
+        return 1080;
     }
+  }
 
-    load(id) {
-        instance.playingVideoId = id
+  setFullscreenIfNecessary() {
+    if (!this.isFullscreen_) this.requestFullscreen();
+  }
 
-        // First, remove all previous event listeners - if any
-        instance.video().off('ended', instance.waitAndFinish)
-        instance.video().off('play', instance.setFullscreenIfNecessary)
-        instance.video().off('fullscreenchange', instance.pauseOnFullscreenExit)
+  pauseOnFullscreenExit() {
+    if (!this.isFullscreen_) instance.pause();
+  }
 
-        // Always start new loaded videos from the beginning
-        instance.video().currentTime(0)
+  addSkipBtn() {
+    if (instance.hasSkipBtn()) return;
 
-        // Set texture when starting directly on a video task type
-        if (instance.portalScreen.material.map != instance.resources.customTextureItems[id])
-            instance.setTexture(id)
-
-        const videoQuality = instance.getVideoQuality()
-        instance.resources.videoPlayers[id].setVideoQuality(videoQuality)
-
-        instance.video().on('play', instance.setFullscreenIfNecessary)
-        instance.video().on('fullscreenchange', instance.pauseOnFullscreenExit)
-        instance.video().on('ended', instance.waitAndFinish)
-
-        instance.focus()
-        instance.addSkipBtn()
-    }
-
-    setTexture(id) {
-        if (!instance.resources.customTextureItems.hasOwnProperty(id))
-            return
-
-        instance.portalScreen.material.map = instance.resources.customTextureItems[id]
-        instance.portalScreen.material.map.flipY = false
-        instance.portalScreen.material.needsUpdate = true
-    }
-
-    //#region Actions
-
-    play() {
-        instance.video().play()
-        instance.experience.videoIsPlaying = true
-    }
-
-    pause() {
-        instance.video().pause()
-        instance.experience.videoIsPlaying = false
-    }
-
-    focus() {
-        instance.camera.zoomIn(2000)
-        instance.tablet.material.map.image.play()
-
-        instance.audio.setOtherAudioIsPlaying(true)
-        instance.audio.fadeOutBgMusic()
-
-        new TWEEN.Tween(instance.portalScreen.material)
-            .to({ color: new THREE.Color(0xFFFFFF) }, 1000)
-            .easing(TWEEN.Easing.Quadratic.InOut)
-            .start()
-    }
-
-    defocus() {
-        if (instance.video()) {
-            instance.pause()
-            instance.tablet.material.map.image.pause()
-            instance.portalScreen.scale.set(0, 0, 0)
-            instance.videoPlayIcon.visible = false
-
-            if (instance.video().isFullscreen_)
-                instance.video().exitFullscreen()
-
-            instance.audio.setOtherAudioIsPlaying(false)
-            instance.audio.fadeInBgMusic()
-        }
-
-        // instance.experience.navigation.next.disabled = false
-    }
-
-    waitAndFinish() {
-        setTimeout(() => { instance.finish() }, 1000)
-    }
-
-    finish() {
-        if (instance.hasSkipBtn())
-            instance.getSkipBtn().remove()
-
-        instance.defocus()
-        instance.world.program.nextStep()
-        setTimeout(() => { instance.playingVideoId = null }, 1000)
-    }
-
-    //#endregion
-
-    getVideoQuality() {
-        switch (instance.world.selectedQuality) {
-            case 'low':
-                return 270
-
-            case 'medium':
-                return 540
-
-            case 'high':
-            default:
-                return 1080
-        }
-    }
-
-    setFullscreenIfNecessary() {
-        if (!this.isFullscreen_)
-            this.requestFullscreen()
-    }
-
-    pauseOnFullscreenExit() {
-        if (!this.isFullscreen_)
-            instance.pause()
-    }
-
-    addSkipBtn() {
-        if (instance.hasSkipBtn())
-            return
-
-        const skipVideo = document.createElement('div')
-        skipVideo.className = 'skip-video btn default less-focused'
-        skipVideo.innerText = _s.miniGames.skip
-        skipVideo.addEventListener('click', instance.finish)
-        instance.videoJsEl().appendChild(skipVideo)
-    }
+    const skipVideo = document.createElement('div');
+    skipVideo.className = 'skip-video btn default less-focused';
+    skipVideo.innerText = _s.miniGames.skip;
+    skipVideo.addEventListener('click', instance.finish);
+    instance.videoJsEl().appendChild(skipVideo);
+  }
 }
 
 const PIXEL_RATIO = (function () {
-    var ctx = document.createElement('canvas').getContext('2d'),
-        dpr = window.devicePixelRatio || 1,
-        bsr = ctx.webkitBackingStorePixelRatio ||
-            ctx.mozBackingStorePixelRatio ||
-            ctx.msBackingStorePixelRatio ||
-            ctx.oBackingStorePixelRatio ||
-            ctx.backingStorePixelRatio || 1;
-    return dpr / bsr;
+  var ctx = document.createElement('canvas').getContext('2d'),
+    dpr = window.devicePixelRatio || 1,
+    bsr =
+      ctx.webkitBackingStorePixelRatio ||
+      ctx.mozBackingStorePixelRatio ||
+      ctx.msBackingStorePixelRatio ||
+      ctx.oBackingStorePixelRatio ||
+      ctx.backingStorePixelRatio ||
+      1;
+  return dpr / bsr;
 })();
 
 const createRetinaCanvas = function (w, h, ratio) {
-    if (!ratio) { ratio = PIXEL_RATIO; }
-    var can = document.createElement('canvas');
-    can.width = w * ratio;
-    can.height = h * ratio;
-    can.style.width = w + 'px';
-    can.style.height = h + 'px';
-    can.getContext('2d').setTransform(ratio, 0, 0, ratio, 0, 0);
-    return can;
-}
+  if (!ratio) {
+    ratio = PIXEL_RATIO;
+  }
+  var can = document.createElement('canvas');
+  can.width = w * ratio;
+  can.height = h * ratio;
+  can.style.width = w + 'px';
+  can.style.height = h + 'px';
+  can.getContext('2d').setTransform(ratio, 0, 0, ratio, 0, 0);
+  return can;
+};
