fab739c433d25b86d12e72939899afbdc7ab00e6
Bogdan Niculae <bogdaniculae@gmail.com>
2024-01-08 11:05:32 +0200

    adjust height images

--- src/Experience/Components/ConfirmationScreen.js
+++ src/Experience/Components/ConfirmationScreen.js
@@ -1,43 +1,43 @@
-import Offline from "../Utils/Offline.js"
-import Experience from "../Experience.js"
-import _s from "../Utils/Strings.js"
-import _gl from "../Utils/Globals.js"
-import _e from "../Utils/Events.js"
+import Offline from '../Utils/Offline.js';
+import Experience from '../Experience.js';
+import _s from '../Utils/Strings.js';
+import _gl from '../Utils/Globals.js';
+import _e from '../Utils/Events.js';
 
-let instance = null
+let instance = null;
 
 export default class ConfirmationScreen {
   constructor() {
-    instance = this
+    instance = this;
 
-    instance.experience = new Experience()
-    instance.debug = instance.experience.debug
-    instance.offline = new Offline()
+    instance.experience = new Experience();
+    instance.debug = instance.experience.debug;
+    instance.offline = new Offline();
   }
 
   show() {
-    instance.world = instance.experience.world
-    instance.program = instance.world.program
-    instance.stepData = instance.program.getCurrentStepData()
-    instance.data = instance.stepData.confirmation_screen
-    
-    instance.setHtml()
-    instance.useCorrectAssetsSrc()
+    instance.world = instance.experience.world;
+    instance.program = instance.world.program;
+    instance.stepData = instance.program.getCurrentStepData();
+    instance.data = instance.stepData.confirmation_screen;
 
-    document.addEventListener(_e.ACTIONS.STEP_TOGGLED, instance.destroy)
+    instance.setHtml();
+    instance.useCorrectAssetsSrc();
+
+    document.addEventListener(_e.ACTIONS.STEP_TOGGLED, instance.destroy);
   }
 
   useCorrectAssetsSrc() {
-    instance.offline.fetchChapterAsset(instance.data, "cs_image", (data) => {
-      document.querySelector(".game-tutorial img").src = data.cs_image
-    })
+    instance.offline.fetchChapterAsset(instance.data, 'cs_image', (data) => {
+      document.querySelector('.game-tutorial img').src = data.cs_image;
+    });
   }
 
   setHtml() {
     const startGame = _gl.elementFromHtml(`
       <button class="btn default focused pulsate">${instance.data.button}</button>
-    `)
-    startGame.addEventListener("click", instance.program.nextStep)
+    `);
+    startGame.addEventListener('click', instance.program.nextStep);
 
     const task = _gl.elementFromHtml(`
       <section class="task">
@@ -47,35 +47,31 @@ export default class ConfirmationScreen {
               <h2>${instance.stepData.details.title}</h2>
             </header>
             <div class="game-tutorial">
-                <img src="${ instance.data.image }" width="100%" height="100%" />
+                <img src="${instance.data.image}" width="100%" height="100%" class="h-full" />
             </div>
-            <div class="game-description">
-              ${
-                instance.stepData.details.prompts
-                  ? instance.stepData.details.prompts[0].prompt
-                  : ""
-              }
+            <div class="game-description text-white mb-2">
+              ${instance.stepData.details.prompts ? instance.stepData.details.prompts[0].prompt : ''}
             </div>
           </div>
         </div>
         <div class="overlay"></div>
       </section>
-    `)
+    `);
 
-    task.querySelector(".content").append(startGame)
-    document.querySelector(".ui-container").append(task)
+    task.querySelector('.content').append(startGame);
+    document.querySelector('.ui-container').append(task);
 
-    instance.experience.navigation.next.classList.remove("focused")
-    instance.experience.navigation.next.innerHTML = _s.miniGames.skip
-    instance.experience.navigation.next.classList.add("less-focused")
-    instance.experience.navigation.container.style.display = "flex"
+    instance.experience.navigation.next.classList.remove('focused');
+    instance.experience.navigation.next.innerHTML = _s.miniGames.skip;
+    instance.experience.navigation.next.classList.add('less-focused');
+    instance.experience.navigation.container.style.display = 'flex';
   }
 
   destroy() {
-    document.querySelector("section.task")?.remove()
+    document.querySelector('section.task')?.remove();
 
-    instance.experience.navigation.next.classList.add("focused")
-    instance.experience.navigation.next.classList.remove("less-focused")
-    instance.experience.navigation.next.innerHTML = instance.experience.icons.next
+    instance.experience.navigation.next.classList.add('focused');
+    instance.experience.navigation.next.classList.remove('less-focused');
+    instance.experience.navigation.next.innerHTML = instance.experience.icons.next;
   }
 }
--- src/Experience/Games/FlipCards.js
+++ src/Experience/Games/FlipCards.js
@@ -53,9 +53,9 @@ export default class FlipCards {
               <h2>${instance.confirmationScreen.cs_title}</h2>
             </header>
             <div class="game-tutorial">
-                <img src="${instance.confirmationScreen.cs_image}" width="100%" height="100%" />
+                <img src="${instance.confirmationScreen.cs_image}" width="100%" height="100%" class="h-full" />
             </div>
-            <div class="game-description">
+            <div class="game-description text-white mb-2">
               ${instance.confirmationScreen.cs_description}
             </div>
           </div>
@@ -74,50 +74,28 @@ export default class FlipCards {
   }
 
   useCorrectAssetsSrcConfirmationScreen() {
-    instance.offline.fetchChapterAsset(
-      instance.confirmationScreen,
-      'cs_image',
-      (data) => {
-        document.querySelector('.game-tutorial img').src = data.cs_image;
-      },
-    );
+    instance.offline.fetchChapterAsset(instance.confirmationScreen, 'cs_image', (data) => {
+      document.querySelector('.game-tutorial img').src = data.cs_image;
+    });
   }
 
   setConfirmationScreenEventListeners() {
-    instance.experience.navigation.prev.removeEventListener(
-      'click',
-      instance.program.previousStep,
-    );
-    instance.experience.navigation.prev.addEventListener(
-      'click',
-      instance.backToGameDescription,
-    );
-    instance.experience.navigation.next.addEventListener(
-      'click',
-      instance.toggleFlipCards,
-    );
+    instance.experience.navigation.prev.removeEventListener('click', instance.program.previousStep);
+    instance.experience.navigation.prev.addEventListener('click', instance.backToGameDescription);
+    instance.experience.navigation.next.addEventListener('click', instance.toggleFlipCards);
   }
 
   backToGameDescription() {
     instance.destroyConfirmationScreen();
     instance.program.gameDescription.show();
-    instance.experience.navigation.prev.addEventListener(
-      'click',
-      instance.program.previousStep,
-    );
+    instance.experience.navigation.prev.addEventListener('click', instance.program.previousStep);
   }
 
   destroyConfirmationScreen() {
     document.querySelector('section.task')?.remove();
 
-    instance.experience.navigation.prev.removeEventListener(
-      'click',
-      instance.backToGameDescription,
-    );
-    instance.experience.navigation.next.removeEventListener(
-      'click',
-      instance.toggleFlipCards,
-    );
+    instance.experience.navigation.prev.removeEventListener('click', instance.backToGameDescription);
+    instance.experience.navigation.next.removeEventListener('click', instance.toggleFlipCards);
   }
 
   toggleFlipCards() {
@@ -187,24 +165,17 @@ export default class FlipCards {
     instance.flipCards.cards.forEach((card, index) => {
       instance.offline.fetchChapterAsset(card, 'image_back', (data) => {
         card.image_back = data.image_back;
-        document.querySelectorAll('article.card .card-back')[
-          index
-        ].style.backgroundImage = "url('" + data.image_back + "')";
+        document.querySelectorAll('article.card .card-back')[index].style.backgroundImage = "url('" + data.image_back + "')";
       });
       instance.offline.fetchChapterAsset(card, 'image_front', (data) => {
         card.image_front = data.image_front;
-        document.querySelectorAll('article.card .card-front')[
-          index
-        ].style.backgroundImage = "url('" + data.image_front + "')";
+        document.querySelectorAll('article.card .card-front')[index].style.backgroundImage = "url('" + data.image_front + "')";
       });
     });
   }
 
   setFlipCardsEventListeners() {
-    instance.experience.navigation.prev.addEventListener(
-      'click',
-      instance.toggleConfirmationScreen,
-    );
+    instance.experience.navigation.prev.addEventListener('click', instance.toggleConfirmationScreen);
 
     const cards = gsap.utils.toArray('.flip-card .card');
     cards.forEach((card, index) => {
@@ -222,13 +193,10 @@ export default class FlipCards {
 
       gsap.set(cFront, { rotationY: 180 });
 
-      const flipAnimation = gsap
-        .timeline({ paused: true })
-        .to(cImage[0], { duration: 1, rotationY: 180 });
+      const flipAnimation = gsap.timeline({ paused: true }).to(cImage[0], { duration: 1, rotationY: 180 });
 
       cInput[0].addEventListener('input', (e) => {
-        if (e.target.value.length > e.target.maxLength)
-          e.target.value = e.target.value.slice(0, e.target.maxLength);
+        if (e.target.value.length > e.target.maxLength) e.target.value = e.target.value.slice(0, e.target.maxLength);
 
         if (e.target.value.length == e.target.maxLength) {
           if (e.target.value == instance.flipCards.cards[index].code) {
@@ -246,12 +214,9 @@ export default class FlipCards {
 
             if (flippedCards.length == instance.flipCards.cards.length) {
               instance.experience.navigation.container.style.display = 'flex';
-              instance.experience.navigation.next.classList.remove(
-                'less-focused',
-              );
+              instance.experience.navigation.next.classList.remove('less-focused');
               instance.experience.navigation.next.classList.add('focused');
-              instance.experience.navigation.next.innerHTML =
-                instance.experience.icons.next;
+              instance.experience.navigation.next.innerHTML = instance.experience.icons.next;
             }
           } else {
             e.target.parentNode.classList.add('wrong-code');
@@ -277,22 +242,15 @@ export default class FlipCards {
 
     instance.experience.navigation.next.classList.add('focused');
     instance.experience.navigation.next.classList.remove('less-focused');
-    instance.experience.navigation.next.innerHTML =
-      instance.experience.icons.next;
+    instance.experience.navigation.next.innerHTML = instance.experience.icons.next;
 
-    instance.experience.navigation.prev.removeEventListener(
-      'click',
-      instance.toggleConfirmationScreen,
-    );
+    instance.experience.navigation.prev.removeEventListener('click', instance.toggleConfirmationScreen);
   }
 
   destroy() {
     instance.destroyConfirmationScreen();
     instance.destroyFlipCards();
-    instance.experience.navigation.prev.addEventListener(
-      'click',
-      instance.program.previousStep,
-    );
+    instance.experience.navigation.prev.addEventListener('click', instance.program.previousStep);
     document.removeEventListener(_e.ACTIONS.STEP_TOGGLED, instance.destroy);
   }
 }
