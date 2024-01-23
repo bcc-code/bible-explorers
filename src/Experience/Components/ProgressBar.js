3a2060b778b31430a8010dba8554d5a746068acb
Bogdan Niculae <bogdaniculae@gmail.com>
2024-01-23 11:02:07 +0200

    add opacity on progress/menu buttons

--- src/Experience/Components/ProgressBar.js
+++ src/Experience/Components/ProgressBar.js
@@ -23,9 +23,7 @@ export default class ProgressBar {
 
     instance.el = {
       passed: instance.htmlEl.querySelector('.percentageBar .passed'),
-      checkpoints: instance.htmlEl.querySelectorAll(
-        "[aria-label='checkpoint']:not(:last-child)",
-      ),
+      checkpoints: instance.htmlEl.querySelectorAll("[aria-label='checkpoint']:not(:last-child)"),
     };
 
     instance.el.checkpoints.forEach(function (checkpoint, index) {
@@ -34,8 +32,7 @@ export default class ProgressBar {
         instance.program.goToCheckpoint(clickedCheckpoint);
       });
 
-      if (index == instance.program.currentCheckpoint)
-        checkpoint.setAttribute('currentCheckpoint', '');
+      if (index == instance.program.currentCheckpoint) checkpoint.setAttribute('currentCheckpoint', '');
     });
   }
 
@@ -48,21 +45,15 @@ export default class ProgressBar {
   }
 
   refresh() {
-    instance.el.passed.style.width =
-      instance.checkpointWidth * instance.program.currentCheckpoint + '%';
+    instance.el.passed.style.width = instance.checkpointWidth * instance.program.currentCheckpoint + '%';
 
     instance.el.checkpoints.forEach((checkpoint) => {
       checkpoint.removeAttribute('currentCheckpoint');
     });
 
     if (instance.program.currentCheckpoint < instance.el.checkpoints.length) {
-      instance.el.checkpoints[
-        instance.program.currentCheckpoint
-      ].classList.remove('locked');
-      instance.el.checkpoints[instance.program.currentCheckpoint].setAttribute(
-        'currentCheckpoint',
-        '',
-      );
+      instance.el.checkpoints[instance.program.currentCheckpoint].classList.remove('locked');
+      instance.el.checkpoints[instance.program.currentCheckpoint].setAttribute('currentCheckpoint', '');
     }
   }
 
@@ -71,46 +62,32 @@ export default class ProgressBar {
   }
 
   static HTML() {
-    let generatedHTML = `<div class="percentageBar">
-                <div class="passed" style="width: ${
-                  instance.checkpointWidth * instance.program.currentCheckpoint
-                }%"></div>
+    let generatedHTML = `<div class="percentageBar transition opacity-0 group-hover/progress-bar:opacity-100">
+                <div class="passed" style="width: ${instance.checkpointWidth * instance.program.currentCheckpoint}%"></div>
             </div>
             <div class="checkpoints">`;
 
     for (let i = 0; i < instance.program.totalCheckpoints; i++) {
-      if (
-        instance.program.programData[i].steps.some(
-          (step) => step.details.step_type == 'video',
-        )
-      ) {
-        generatedHTML += `<button class="btn rounded focused" aria-label="checkpoint" data-index="${i}">
+      if (instance.program.programData[i].steps.some((step) => step.details.step_type == 'video')) {
+        generatedHTML += `<button class="btn rounded focused opacity-50 group-hover/progress-bar:opacity-100" aria-label="checkpoint" data-index="${i}">
                         <svg class="film-icon icon"viewBox="0 0 24 22">
                             <use href="#film"></use>
                         </svg>
                     </button>`;
-      } else if (
-        instance.program.programData[i].steps.some(
-          (step) => step.details.step_type == 'quiz',
-        )
-      ) {
-        generatedHTML += `<button class="btn rounded focused" aria-label="checkpoint" data-index="${i}">
+      } else if (instance.program.programData[i].steps.some((step) => step.details.step_type == 'quiz')) {
+        generatedHTML += `<button class="btn rounded focused opacity-50 group-hover/progress-bar:opacity-100" aria-label="checkpoint" data-index="${i}">
                         <svg class="question-mark-icon icon" viewBox="0 0 15 22">
                             <use href="#question-mark"></use>
                         </svg>
                     </button>`;
-      } else if (
-        instance.program.programData[i].steps.some(
-          (step) => step.details.step_type == 'pause',
-        )
-      ) {
-        generatedHTML += `<button class="btn rounded focused" aria-label="checkpoint" data-index="${i}">
+      } else if (instance.program.programData[i].steps.some((step) => step.details.step_type == 'pause')) {
+        generatedHTML += `<button class="btn rounded focused opacity-50 group-hover/progress-bar:opacity-100" aria-label="checkpoint" data-index="${i}">
                         <svg class="pause-icon icon" viewBox="0 0 15 18">
                             <use href="#pause"></use>
                         </svg>
                     </button>`;
       } else {
-        generatedHTML += `<button class="btn rounded focused" aria-label="checkpoint" data-index="${i}">
+        generatedHTML += `<button class="btn rounded focused opacity-50 group-hover/progress-bar:opacity-100" aria-label="checkpoint" data-index="${i}">
                     <svg class="task-icon icon" viewBox="0 0 25 25">
                         <use href="#pen-to-square"></use>
                     </svg>
@@ -118,7 +95,7 @@ export default class ProgressBar {
       }
     }
 
-    generatedHTML += `<button class="btn rounded focused" aria-label="checkpoint">
+    generatedHTML += `<button class="btn rounded focused opacity-50 group-hover/progress-bar:opacity-100" aria-label="checkpoint">
                 <svg class="star-icon icon" viewBox="0 0 29 29">
                     <use href="#star"></use>
                 </svg>
