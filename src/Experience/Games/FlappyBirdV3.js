98dfd85f63edf4e0e88e0662ea5690119f088c06
Bogdan Niculae <bogdaniculae@gmail.com>
2024-02-08 12:34:38 +0200

    add game over on top/bottom collision

--- src/Experience/Games/FlappyBirdV3.js
+++ src/Experience/Games/FlappyBirdV3.js
@@ -1,5 +1,5 @@
 class Player {
-    constructor(canvas) {
+    constructor(canvas, gameOverCallback) {
         this.canvas = canvas
         this.ctx = canvas.getContext('2d')
         this.width = 32
@@ -10,6 +10,7 @@ class Player {
         this.gravity = 0.4
         this.jumpStrength = -6
         this.spacePressed = false
+        this.gameOverCallback = gameOverCallback // Callback function for game over
 
         // Add event listeners for keydown and keyup
         document.addEventListener('keydown', this.handleKeyDown.bind(this))
@@ -25,9 +26,11 @@ class Player {
         if (this.y < 0) {
             this.y = 0 // Prevent player from going above the top edge
             this.velocityY = 0 // Stop upward velocity
+            this.gameOverCallback() // Notify game over
         } else if (this.y + this.height > this.canvas.height) {
             this.y = this.canvas.height - this.height // Prevent player from going below the bottom edge
             this.velocityY = 0 // Stop downward velocity
+            this.gameOverCallback() // Notify game over
         }
 
         // Draw the player
@@ -95,8 +98,14 @@ class FlappyBird {
         // Player instance (will be added when "Start" is clicked)
         this.player = null
 
-        // Game loop variable
+        // Game over flag
+        this.gameOver = false
+
+        // Store the requestAnimationFrame ID
         this.gameLoop = null
+
+        // Flag to indicate whether the game has started
+        this.gameStarted = false
     }
 
     resizeCanvas() {
@@ -144,24 +153,26 @@ class FlappyBird {
     }
 
     handleClick(mouseX, mouseY) {
-        // Check if the mouse click is within any menu option
-        const centerX = this.canvas.width / 2
-        const centerY = this.canvas.height / 2
-        const startTextWidth = this.ctx.measureText('Start').width
-        const exitTextWidth = this.ctx.measureText('Exit').width
+        if (!this.gameStarted) {
+            // Check if the mouse click is within any menu option only if the game has not started
+            const centerX = this.canvas.width / 2
+            const centerY = this.canvas.height / 2
+            const startTextWidth = this.ctx.measureText('Start').width
+            const exitTextWidth = this.ctx.measureText('Exit').width
 
-        const startTextX = centerX - startTextWidth / 2
-        const startTextY = centerY - 20
+            const startTextX = centerX - startTextWidth / 2
+            const startTextY = centerY - 20
 
-        const exitTextX = centerX - exitTextWidth / 2
-        const exitTextY = centerY + 20
+            const exitTextX = centerX - exitTextWidth / 2
+            const exitTextY = centerY + 20
 
-        if (mouseX > startTextX && mouseX < startTextX + startTextWidth && mouseY > startTextY - 30 && mouseY < startTextY) {
-            // Start option clicked
-            this.startGame()
-        } else if (mouseX > exitTextX && mouseX < exitTextX + exitTextWidth && mouseY > exitTextY - 30 && mouseY < exitTextY) {
-            // Exit option clicked
-            this.exitGame()
+            if (mouseX > startTextX && mouseX < startTextX + startTextWidth && mouseY > startTextY - 30 && mouseY < startTextY) {
+                // Start option clicked
+                this.startGame()
+            } else if (mouseX > exitTextX && mouseX < exitTextX + exitTextWidth && mouseY > exitTextY - 30 && mouseY < exitTextY) {
+                // Exit option clicked
+                this.exitGame()
+            }
         }
     }
 
@@ -169,8 +180,14 @@ class FlappyBird {
         // Implement game start logic here
         console.log('Game started!')
 
+        // Set gameStarted flag to true
+        this.gameStarted = true
+
+        // Remove click event listener
+        this.canvas.removeEventListener('click', this.handleClick)
+
         // Create player instance
-        this.player = new Player(this.canvas)
+        this.player = new Player(this.canvas, this.gameOverCallback.bind(this))
 
         // Start the game loop
         this.gameLoop = requestAnimationFrame(this.update.bind(this))
@@ -183,11 +200,38 @@ class FlappyBird {
         // Redraw the background image
         this.drawBackground()
 
-        // Update game state
-        this.player.update()
+        // Update game state if game is not over
+        if (!this.gameOver) {
+            this.player.update()
+        }
 
-        // Request next frame
-        this.gameLoop = requestAnimationFrame(this.update.bind(this))
+        // Request next frame if game is not over
+        if (!this.gameOver) {
+            this.gameLoop = requestAnimationFrame(this.update.bind(this))
+        }
+    }
+
+    gameOverCallback() {
+        // Game over logic
+        this.gameOver = true
+        console.log('Game over!')
+        // Draw game over screen
+        this.drawGameOverScreen()
+        // Stop the game loop
+        cancelAnimationFrame(this.gameLoop)
+    }
+
+    drawGameOverScreen() {
+        // Draw game over screen
+        this.ctx.fillStyle = 'rgba(0, 0, 0, 0.5)'
+        this.ctx.fillRect(0, 0, this.canvas.width, this.canvas.height)
+        this.ctx.fillStyle = 'white'
+        this.ctx.font = '36px Arial'
+        const gameOverText = 'Game Over'
+        const centerX = this.canvas.width / 2
+        const centerY = this.canvas.height / 2
+        const gameOverTextWidth = this.ctx.measureText(gameOverText).width
+        this.ctx.fillText(gameOverText, centerX - gameOverTextWidth / 2, centerY)
     }
 
     exitGame() {
