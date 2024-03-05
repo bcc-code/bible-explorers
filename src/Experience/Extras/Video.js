519b33e16e2379d193a9f8e9c452962082872f02
Bogdan Niculae <bogdaniculae@gmail.com>
2024-03-05 11:05:15 +0200

    Update Video.js

--- src/Experience/Extras/Video.js
+++ src/Experience/Extras/Video.js
@@ -138,23 +138,3 @@ export default class Video {
         instance.videoJsEl().appendChild(skipVideo)
     }
 }
-
-const PIXEL_RATIO = (function () {
-    var ctx = document.createElement('canvas').getContext('2d'),
-        dpr = window.devicePixelRatio || 1,
-        bsr = ctx.webkitBackingStorePixelRatio || ctx.mozBackingStorePixelRatio || ctx.msBackingStorePixelRatio || ctx.oBackingStorePixelRatio || ctx.backingStorePixelRatio || 1
-    return dpr / bsr
-})()
-
-const createRetinaCanvas = function (w, h, ratio) {
-    if (!ratio) {
-        ratio = PIXEL_RATIO
-    }
-    var can = document.createElement('canvas')
-    can.width = w * ratio
-    can.height = h * ratio
-    can.style.width = w + 'px'
-    can.style.height = h + 'px'
-    can.getContext('2d').setTransform(ratio, 0, 0, ratio, 0, 0)
-    return can
-}
