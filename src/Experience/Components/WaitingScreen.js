e4bad79e735a3d7285f46406407b61145fefadbf
Bogdan Niculae <bogdaniculae@gmail.com>
2024-03-19 13:54:47 +0200

    Update WaitingScreen.js

--- src/Experience/Components/WaitingScreen.js
+++ src/Experience/Components/WaitingScreen.js
@@ -14,15 +14,29 @@ export default class WaitingScreen {
     show() {
         instance.world = instance.experience.world
         instance.program = instance.world.program
-        instance.video = instance.program.video
+        // instance.video = instance.program.video
 
-        const id = instance.world.selectedChapter.lobby_video_loop
-        instance.video.load('lobby-video-' + id)
-        instance.video.play()
+        // const id = instance.world.selectedChapter.lobby_video_loop
+        // instance.video.load('lobby-video-' + id)
+        // instance.video.play()
+
+        instance.video = document.createElement('video')
+        instance.video.setAttribute('src', './textures/CR_Loop.mp4')
+        instance.video.setAttribute('playsinline', '')
+        instance.video.setAttribute('muted', '')
+        instance.video.setAttribute('autoplay', '')
+        instance.video.setAttribute('loop', '')
+        instance.video.style.position = 'fixed'
+        instance.video.style.top = '0'
+        instance.video.style.left = '0'
+        instance.video.style.width = '100%'
+        instance.video.style.height = '100%'
+        instance.video.style.objectFit = 'cover'
+        instance.video.style.zIndex = '-1'
 
         if (document.querySelector('#childrenNames')) return
 
-        const wrapper = _gl.elementFromHtml(`<div class="fixed inset-0 bg-black/70" id="waitingScreen"></div>`)
+        const wrapper = _gl.elementFromHtml(`<div class="fixed inset-0 bg-black isolate" id="waitingScreen"></div>`)
         const form = _gl.elementFromHtml(
             `<form id="childrenNames" class="max-w-screen-sm absolute bottom-6 left-1/2 -translate-x-1/2">
                 <input class="w-full h-12 bg-white text-bke-darkpurple outline-none text-lg px-4 mb-4"/>
@@ -30,9 +44,9 @@ export default class WaitingScreen {
             </form>`
         )
 
-        const nameLabelContainer = _gl.elementFromHtml('<ul class="absolute w-full h-full" id="names-label"></ul>')
+        const nameLabelContainer = _gl.elementFromHtml('<ul class="absolute w-full h-full pointer-events-none p-4" id="names-label"></ul>')
 
-        wrapper.append(form, nameLabelContainer)
+        wrapper.append(instance.video, form, nameLabelContainer)
 
         document.querySelector('#chapter-wrapper').append(wrapper)
 
@@ -81,16 +95,20 @@ export default class WaitingScreen {
     }
 
     destroy() {
-        // Remove form event listeners
         const form = document.querySelector('#childrenNames')
         form.querySelector('button').removeEventListener('click', instance.handleFormSubmission)
         form.querySelector('input').removeEventListener('keyup', instance.handleFormSubmission)
 
-        // Remove all nameLabel event listeners
         const removeButtons = document.querySelector('#chapter-wrapper').querySelectorAll('span')
         removeButtons.forEach((removeButton) => {
             removeButton.removeEventListener('click', instance.handleRemoveName)
         })
+
+        // Remove the video element
+        if (instance.video) {
+            instance.video.pause() // Pause the video
+            instance.video.remove() // Remove the video element from the DOM
+        }
 
         // Remove all elements appended to smallScreen
         document.querySelector('#waitingScreen').remove()
