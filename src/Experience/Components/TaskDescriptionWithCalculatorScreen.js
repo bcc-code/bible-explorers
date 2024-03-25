7a8a743bcc2996247de87ce846453842092f059d
Simon Costea <simoncostea@yahoo.com>
2024-03-25 18:30:44 +0200

    Fix full height image on task description

--- src/Experience/Components/TaskDescriptionWithCalculatorScreen.js
+++ src/Experience/Components/TaskDescriptionWithCalculatorScreen.js
@@ -43,7 +43,7 @@ export default class TaskDescriptionWithCalculatorScreen {
                 <div class="relative mx-auto max-w-[1980px] px-4 pb-4 pt-24 tv:gap-8 tv:px-8 tv:pt-32">
                     <h1 class="text-2xl tv:text-3xl font-bold text-center mb-4">${instance.data.td_title !== '' ? instance.data.td_title : ''}</h1>
                     ${instance.data.td_description ? `<p class="text-xl text-center">${instance.data.td_description}</p>` : ''}
-                    ${instance.data.td_image ? `<div class="aspect-video max-w-[600px] mt-8 mx-auto" id="task-image"><img src="${instance.data.td_image}" width="100%" height="100%" class="h-full" /></div>` : ''}
+                    ${instance.data.td_image ? `<div class="aspect-video max-w-[600px] mt-8 mx-auto" id="task-image"><img src="${instance.data.td_image}" width="100%" height="100%" /></div>` : ''}
                     ${instance.data.td_button !== '' ? `<div class="flex justify-center mt-8"><button class="button-normal">${instance.data.td_button}</button></div>` : ''}
                 </div>
             </div>`
