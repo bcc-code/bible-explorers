47c39b1cd3f9aa1393d1bbf33ae0838f650d6d45
Bogdan Niculae <bogdaniculae@gmail.com>
2024-03-25 16:40:31 +0200

    Update WaitingScreen.js

--- src/Experience/Components/WaitingScreen.js
+++ src/Experience/Components/WaitingScreen.js
@@ -14,28 +14,28 @@ export default class WaitingScreen {
     show() {
         instance.world = instance.experience.world
         instance.program = instance.world.program
-        // instance.video = instance.program.video
+        instance.video = instance.program.video
 
         instance.experience.maxVW = 36
         instance.experience.adjustScreensWrapperSize()
 
-        // const id = instance.world.selectedChapter.lobby_video_loop
-        // instance.video.load('lobby-video-' + id)
-        // instance.video.play()
+        const id = instance.world.selectedChapter.lobby_video_loop
+        instance.video.load('lobby-video-' + id)
+        instance.video.play()
 
-        instance.video = document.createElement('video')
-        instance.video.setAttribute('src', './textures/Waitingscreen V003.mp4')
-        instance.video.setAttribute('playsinline', '')
-        instance.video.setAttribute('muted', '')
-        instance.video.setAttribute('autoplay', '')
-        instance.video.setAttribute('loop', '')
-        instance.video.style.position = 'fixed'
-        instance.video.style.top = '0'
-        instance.video.style.left = '0'
-        instance.video.style.width = '100%'
-        instance.video.style.height = '100%'
-        instance.video.style.objectFit = 'cover'
-        instance.video.style.zIndex = '-1'
+        instance.videoBG = document.createElement('video')
+        instance.videoBG.setAttribute('src', './textures/Waitingscreen V003.mp4')
+        instance.videoBG.setAttribute('playsinline', '')
+        instance.videoBG.setAttribute('muted', '')
+        instance.videoBG.setAttribute('autoplay', '')
+        instance.videoBG.setAttribute('loop', '')
+        instance.videoBG.style.position = 'fixed'
+        instance.videoBG.style.top = '0'
+        instance.videoBG.style.left = '0'
+        instance.videoBG.style.width = '100%'
+        instance.videoBG.style.height = '100%'
+        instance.videoBG.style.objectFit = 'cover'
+        instance.videoBG.style.zIndex = '-1'
 
         if (document.querySelector('#childrenNames')) return
 
@@ -49,7 +49,7 @@ export default class WaitingScreen {
 
         const nameLabelContainer = _gl.elementFromHtml('<ul class="p-4 bg-black/40 overflow-y-auto h-full" id="names-label"></ul>')
 
-        wrapper.append(instance.video, form)
+        wrapper.append(instance.videoBG, form)
         instance.experience.interface.helperScreen.append(nameLabelContainer)
 
         document.querySelector('#chapter-wrapper').prepend(wrapper)
@@ -127,10 +127,11 @@ export default class WaitingScreen {
         instance.removeEventListeners()
 
         // Remove the video element
-        if (instance.video) {
-            instance.video.pause() // Pause the video
-            instance.video.remove() // Remove the video element from the DOM
+        if (instance.videoBG) {
+            instance.videoBG.pause()
+            instance.videoBG.remove()
         }
+        instance.video?.defocus()
 
         // Remove all elements appended to smallScreen
         document.querySelector('#waitingScreen').remove()
