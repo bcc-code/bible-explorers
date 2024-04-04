91773c51f623e9a8ee3590436f0cd6b4062312db
Simon Costea <simoncostea@yahoo.com>
2024-04-04 13:11:24 +0300

    Rename waiting screen video

--- src/Experience/Components/WaitingScreen.js
+++ src/Experience/Components/WaitingScreen.js
@@ -27,7 +27,7 @@ export default class WaitingScreen {
         instance.video.play()
 
         instance.videoBG = document.createElement('video')
-        instance.videoBG.setAttribute('src', './textures/Waitingscreen V003.mp4')
+        instance.videoBG.setAttribute('src', './textures/Waitingscreen_V003.mp4')
         instance.videoBG.setAttribute('playsinline', '')
         // instance.videoBG.setAttribute('muted', '')
         instance.videoBG.setAttribute('autoplay', '')
