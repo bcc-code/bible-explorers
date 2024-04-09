0702d98df9266b68cf581271d4d523c3dea7576b
Simon Costea <simoncostea@yahoo.com>
2024-04-09 09:51:39 +0300

    Workflow updates for the 3D branch

--- src/Experience/Utils/Api.js
+++ src/Experience/Utils/Api.js
@@ -1,26 +1,25 @@
-import _lang from '../Utils/Lang.js';
+import _lang from "../Utils/Lang.js";
 
-const wpApiUrl_production = 'https://biblekids.io/wp-json';
-const wpApiUrl_staging = 'https://staging-bcckids.kinsta.cloud/wp-json';
-const wpApiUrl_local = 'https://bcckids.local/wp-json';
+const wpApiUrl_production = "https://biblekids.io/wp-json";
+const wpApiUrl_staging = "https://staging-bcckids.kinsta.cloud/wp-json";
+const wpApiUrl_local = "https://bcckids.local/wp-json";
 const wpApiUrl = getWpApiUrl();
 
 const getBiexChapters = () =>
-  wpApiUrl + '/biex-chapters/get?lang=' + _lang.getLanguageCode();
-const getRoles = (personId) => wpApiUrl_production + '/roles/' + personId;
-const saveAnswer = () => wpApiUrl + '/answer/save';
+  wpApiUrl + "/biex-chapters/get?lang=" + _lang.getLanguageCode();
+const getRoles = (personId) => wpApiUrl_production + "/roles/" + personId;
+const saveAnswer = () => wpApiUrl + "/answer/save";
 
 export default { getBiexChapters, getRoles, saveAnswer };
 
 function getWpApiUrl() {
   switch (window.location.hostname) {
-    case 'explorers.biblekids.io':
+    case "explorers.biblekids.io":
+    case "zealous-ground-0c9103f03-3d.1.azurestaticapps.net":
+    case "zealous-ground-0c9103f03-3d.westeurope.1.azurestaticapps.net":
       return wpApiUrl_production;
 
-    case 'zealous-ground-0c9103f03-develop.westeurope.1.azurestaticapps.net':
-      return wpApiUrl_staging;
-
-    case 'localhost':
+    case "localhost":
       return wpApiUrl_staging;
       return wpApiUrl_local;
 
