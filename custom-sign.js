131b89abb27c89a9b42e3e77f372b5fda74bcf9e
Simon Costea <simoncostea@yahoo.com>
2024-05-07 18:35:09 +0300

    One liner for Azure Sign Tool

--- custom-sign.js
+++ custom-sign.js
@@ -5,21 +5,12 @@ module.exports = async function (configuration) {
     const directoryId = process.env.WINDOWS_DIRECTORY_ID
     const clientId = process.env.WINDOWS_CLIENT_ID
     const clientSecret = process.env.WINDOWS_CLIENT_SECRET
-    const signCommand = `
-        AzureSignTool sign /debug \
-            -du "https://explorers.biblekids.io" \
-            -kvu "https://bccm-code-sign2.vault.azure.net" \
-            -kvt ${directoryId} \
-            -kvi ${clientId} \
-            -kvs ${clientSecret} \
-            -kvc "HSM-CS" \
-            -tr "http://timestamp.digicert.com" \
-            -v ${filePath}
-    `
 
     try {
-        console.log(`Signing ${filePath} with Azure Key Vault certificate`)
-        execSync(signCommand, { stdio: 'inherit' })
+        execSync(
+            `AzureSignTool sign -du "https://explorers.biblekids.io" -kvu "https://bccm-code-sign2.vault.azure.net" -kvt ${directoryId} -kvi ${clientId} -kvs ${clientSecret} -kvc "HSM-CS" -tr "http://timestamp.digicert.com" -v ${filePath}`,
+            { stdio: 'inherit' }
+        )
         console.log(`Successfully signed ${filePath}`)
     } catch (error) {
         console.error(`Failed to sign ${filePath}`)
