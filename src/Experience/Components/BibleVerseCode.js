dda774e794f67387f169b3f06eb350ca22618558
Simon Costea <simoncostea@yahoo.com>
2024-08-09 17:57:23 +0300

    Design updates to Bible verse code task

--- src/Experience/Components/BibleVerseCode.js
+++ src/Experience/Components/BibleVerseCode.js
@@ -56,7 +56,7 @@ export default class BibleVerseCode {
         instance.bibleVerseCode.book.forEach(function (number, index) {
             unlockScreen.querySelector('.bible-book').append(
                 _gl.elementFromHtml(`<div class="flex text-center flex-col">
-                    <input type="number" id="bible-book-${index + 1}" min="0" max="9" />
+                    <input type="number" id="bible-book-${index + 1}" maxlength="1" class="type1" />
                     <label for="bible-book-${index + 1}">${number.icon}</label>
                 </div>`)
             )
@@ -64,7 +64,7 @@ export default class BibleVerseCode {
         instance.bibleVerseCode.chapter.forEach(function (number, index) {
             unlockScreen.querySelector('.bible-chapter').append(
                 _gl.elementFromHtml(`<div class="flex text-center flex-col">
-                    <input type="number" id="bible-chapter-${index + 1}" min="0" max="9" />
+                    <input type="number" id="bible-chapter-${index + 1}" maxlength="1" class="type2" />
                     <label for="bible-chapter-${index + 1}">${number.icon}</label>
                 </div>`)
             )
@@ -72,7 +72,7 @@ export default class BibleVerseCode {
         instance.bibleVerseCode.verse_from.forEach(function (number, index) {
             unlockScreen.querySelector('.bible-verse-from').append(
                 _gl.elementFromHtml(`<div class="flex text-center flex-col">
-                    <input type="number" id="bible-verse-from-${index + 1}" min="0" max="9" />
+                    <input type="number" id="bible-verse-from-${index + 1}" maxlength="1" class="type3" />
                     <label for="bible-verse-from-${index + 1}">${number.icon}</label>
                 </div>`)
             )
@@ -80,7 +80,7 @@ export default class BibleVerseCode {
         instance.bibleVerseCode.verse_to.forEach(function (number, index) {
             unlockScreen.querySelector('.bible-verse-to').append(
                 _gl.elementFromHtml(`<div class="flex text-center flex-col">
-                    <input type="number" id="bible-verse-to-${index + 1}" min="0" max="9" />
+                    <input type="number" id="bible-verse-to-${index + 1}" maxlength="1" class="type3" />
                     <label for="bible-verse-to-${index + 1}">${number.icon}</label>
                 </div>`)
             )
@@ -91,41 +91,40 @@ export default class BibleVerseCode {
         document.querySelector('#check-code').addEventListener('click', instance.checkCode)
         document.querySelector('#glitch-character').addEventListener('click', instance.showOpenGuide)
         document.querySelector('#open-guide').addEventListener('click', instance.popGlitchGuide)
+        instance.goToNextInputListener()
         document.addEventListener(_e.ACTIONS.STEP_TOGGLED, instance.destroy)
     }
 
     checkCode() {
-        let wrong = false
+        document.querySelectorAll('.bible-verse-code__input input').forEach((input) => {
+            input.classList.remove('correct', 'wrong')
+        })
 
         document.querySelectorAll('.bible-book input').forEach((input, index) => {
-            if (input.value != instance.bibleVerseCode.book[index].number) {
-                console.log('book input ' + (index + 1) + ' is wrong')
-                wrong = true
-            }
+            input.classList.add(
+                input.value != instance.bibleVerseCode.book[index].number ? 'wrong' : 'correct'
+            )
         })
 
         document.querySelectorAll('.bible-chapter input').forEach((input, index) => {
-            if (input.value != instance.bibleVerseCode.chapter[index].number) {
-                console.log('chapter input ' + (index + 1) + ' is wrong')
-                wrong = true
-            }
+            input.classList.add(
+                input.value != instance.bibleVerseCode.chapter[index].number ? 'wrong' : 'correct'
+            )
         })
 
         document.querySelectorAll('.bible-verse-from input').forEach((input, index) => {
-            if (input.value != instance.bibleVerseCode.verse_from[index].number) {
-                console.log('verse-from input ' + (index + 1) + ' is wrong')
-                wrong = true
-            }
+            input.classList.add(
+                input.value != instance.bibleVerseCode.verse_from[index].number ? 'wrong' : 'correct'
+            )
         })
 
         document.querySelectorAll('.bible-verse-to input').forEach((input, index) => {
-            if (input.value != instance.bibleVerseCode.verse_to[index].number) {
-                console.log('verse-to input ' + (index + 1) + ' is wrong')
-                wrong = true
-            }
+            input.classList.add(
+                input.value != instance.bibleVerseCode.verse_to[index].number ? 'wrong' : 'correct'
+            )
         })
 
-        if (wrong) {
+        if (document.querySelectorAll('input.wrong').length > 0) {
             instance.audio.playSound('wrong')
         } else {
             instance.audio.playSound('task-completed')
@@ -137,6 +136,49 @@ export default class BibleVerseCode {
             instance.experience.navigation.next.innerText = ''
             instance.experience.navigation.next.className = `button button-arrow`
         }
+    }
+
+    goToNextInputListener() {
+        document.querySelectorAll('.bible-verse-code__input input').forEach((input) => {
+            input.addEventListener('input', (e) => {
+                if (e.target.value.length > e.target.maxLength) {
+                    e.target.value = e.target.value.slice(0, e.target.maxLength)
+                }
+            })
+        })
+
+        document.getElementById('bible-book-1').addEventListener('input', (e) => {
+            if (e.target.value.length == e.target.maxLength) document.getElementById('bible-book-2').focus()
+        })
+
+        document.getElementById('bible-book-2').addEventListener('input', (e) => {
+            if (e.target.value.length == e.target.maxLength)
+                document.getElementById('bible-chapter-1').focus()
+        })
+
+        document.getElementById('bible-chapter-1').addEventListener('input', (e) => {
+            if (e.target.value.length == e.target.maxLength)
+                document.getElementById('bible-verse-from-1').focus()
+        })
+
+        document.getElementById('bible-verse-from-1').addEventListener('input', (e) => {
+            if (e.target.value.length == e.target.maxLength)
+                document.getElementById('bible-verse-from-2').focus()
+        })
+
+        document.getElementById('bible-verse-from-2').addEventListener('input', (e) => {
+            if (e.target.value.length == e.target.maxLength)
+                document.getElementById('bible-verse-to-1').focus()
+        })
+
+        document.getElementById('bible-verse-to-1').addEventListener('input', (e) => {
+            if (e.target.value.length == e.target.maxLength)
+                document.getElementById('bible-verse-to-2').focus()
+        })
+
+        document.getElementById('bible-verse-to-2').addEventListener('input', (e) => {
+            if (e.target.value.length == e.target.maxLength) document.getElementById('check-code').focus()
+        })
     }
 
     showOpenGuide() {
